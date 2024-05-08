d14fd654e8ad1561ac207f1407cabfe47d8fee7d
PhilipovPh <123362219+PhilipovPh@users.noreply.github.com>
2024-05-08 20:40:42 +0300

    Update 3.houseParty.js

--- Arrays Advansed Exercise - 10.02.23/3.houseParty.js
+++ Arrays Advansed Exercise - 10.02.23/3.houseParty.js
@@ -1,3 +1,22 @@
-function list(index) {
-    
+function party(arr) {
+    let partylist = [];
+
+    for (let el of arr) {
+        let comands = el.split(' ');
+        if (comands[2] === 'going!') {
+            partylist.push(comands[0]);
+        } else if (comands[2] === 'not') {
+            if (partylist.includes(comands[0])) {
+                partylist.splice(partylist.indexOf(comands[2]), 1);
+            } else {
+                console.log(`${comands[0]} is not on the list`)
+            }
+        }
+    }
+    console.log(partylist);
 }
+
+party(['Allie is going!',
+    'George is going!',
+    'John is not going!',
+    'George is not going!'])
